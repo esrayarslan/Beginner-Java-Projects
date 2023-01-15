a6f7e6eb4dbd5f0470e82201e60876b905d685b0
Esra Arslan <esrayildizarslan@gmail.com>
2023-01-15 21:02:51 +0300

    Calculating the Leap Year

---
+++ _16Calculating the Leap Year/src/calculatingTheLeapYear.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class calculatingTheLeapYear {
+
+    public static void main(String[] args) {
+
+        System.out.print("Lütfen Hesaplamak İstediğiniz Yılı Giriniz : ");
+        Scanner input = new Scanner(System.in);
+        int year = input.nextInt();
+
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                System.out.println("" + year + " : Artık Bir Yıldır ! ");
+            } else {
+                System.out.println("" + year + " : Artık Bir Yıl Değildir !");
+            }
+        } else if (year % 4 == 0) {
+            System.out.println("" + year + " : Artık Bir Yıldır ! ");
+        } else {
+            System.out.println("" + year + " : Artık Bir Yıl Değildir !");
+        }
+    }
+}
