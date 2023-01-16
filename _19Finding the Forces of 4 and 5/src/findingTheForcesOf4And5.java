efd96724155d3e6f79f1f4aa20f645ccca1c7d7d
Esra Arslan <esrayildizarslan@gmail.com>
2023-01-16 16:34:31 +0300

    Finding the Forces of 4 and 5

--- _19Finding the Forces of 4 and 5/src/findingTheForcesOf4And5.java
+++ _19Finding the Forces of 4 and 5/src/findingTheForcesOf4And5.java
@@ -4,11 +4,19 @@ public class findingTheForcesOf4And5 {
 
     public static void main(String[] args) {
 
+
+
+        /* We are writing the program that prints
+        the powers of 4 and 5 up to the number entered
+        with Java loops on the screen.
+        */
+
         int n;
         Scanner input = new Scanner(System.in);
-        System.out.println("Bir Sayı Giriniz : ");
+        System.out.print("Sınır Sayısını Giriniz : ");
+        n = input.nextInt();
 
-        for (int i = 1; i <= 100; i*=4){
+        for (int i = 1; i <= n; i*=4){
 
         }
     }
