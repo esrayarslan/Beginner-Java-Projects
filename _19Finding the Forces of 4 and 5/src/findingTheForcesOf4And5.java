916cd65857bd4c32c439f58fffde9ce24ddcdcc2
Esra Arslan <esrayildizarslan@gmail.com>
2023-01-16 16:28:57 +0300

    finding the forces of 4 and 5

---
+++ _19Finding the Forces of 4 and 5/src/findingTheForcesOf4And5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class findingTheForcesOf4And5 {
+
+    public static void main(String[] args) {
+
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz : ");
+
+        for (int i = 1; i <= 100; i*=4){
+
+        }
+    }
+}
