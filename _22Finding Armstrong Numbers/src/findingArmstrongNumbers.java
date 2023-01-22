e10f9804b0e65c37b5fa65c0c285bf9c71f0b1f0
Esra Arslan <esrayildizarslan@gmail.com>
2023-01-22 23:42:46 +0300

    Finding Armstrong Numbers

--- _22Finding Armstrong Numbers/src/findingArmstrongNumbers.java
+++ _22Finding Armstrong Numbers/src/findingArmstrongNumbers.java
@@ -1,5 +1,18 @@
 public class findingArmstrongNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*Basamak Sayısı bulma
+
+        int a = 2451, numberOfDigits = 0, numberCounter = 0;
+        System.out.println(a);
+
+        while (a != 0) {
+            a /= 10;
+            System.out.println(a);
+            numberCounter++;
+        } */
+
+        
+
     }
 }
